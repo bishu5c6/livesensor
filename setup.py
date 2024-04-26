07ffd5a348667ba575dfac5ca7e9765ea6004dc5
Biswanth <pinnikabiswanth@gmail.com>
2024-04-26 17:46:23 +0530

    added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from typing import List
+
+PROJECT_NAME ="SENSOR FALUT DETECTION"
+VERSION = "0.0.1"
+DESCRIPTION = "AIR PRESSURE SYSTEM"
+AUTHOR = "BISWANTH PINNIKA"
+AUTHOR_EMAIL = "pinnikabiswanth@gmail.com"
+
+REQUIREMENTS_FILE_NAME = 'requirements.txt'
+
+HYPHON_E_DOT = "-e ."
+
+def get_requirements_list() -> List[str]:
+    with open(REQUIREMENTS_FILE_NAME) as requriment_file:
+        requriment_list = requriment_file.readlines()
+        requriment_list = [ requriment_name.replace("\n", "") for requriment_name in requriment_list]
+
+        if HYPHON_E_DOT in requriment_list:
+            requriment_list.remove(HYPHON_E_DOT)
+        return requriment_list
+
+
+setup(name=PROJECT_NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=AUTHOR_EMAIL,
+      packages=find_packages(),
+      install_requires = get_requirements_list()
+     )
